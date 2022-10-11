c553e67d814e7bd2119bfdae3ff6779a4405d719
color <95304946+HermanMazur@users.noreply.github.com>
2022-10-11 13:09:04 +0300

    b4a9604d912adcda3dc38f0ba7bfa255

--- src/components/servises/Servises.jsx
+++ src/components/servises/Servises.jsx
@@ -3,31 +3,31 @@ import axios from "axios";
 
 
 export async function TrendMoviesFetch (){
-const response =  await axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=125725f49ad2ae69609a1a5a9c4211d9')  
+const response =  await axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=b4a9604d912adcda3dc38f0ba7bfa255')  
 
     return response.data.results;
 }
 
 
 export async function SearchMoviesFetch(input) {
-    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${input}&api_key=125725f49ad2ae69609a1a5a9c4211d9&language=en-US&page=1&include_adult=false`)
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${input}&api_key=b4a9604d912adcda3dc38f0ba7bfa255&language=en-US&page=1&include_adult=false`)
     return response.data.results;
 };
 
 export async function MovieDetalisFetch(id) {
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=125725f49ad2ae69609a1a5a9c4211d9&language=en-US`);
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=b4a9604d912adcda3dc38f0ba7bfa255&language=en-US`);
 
     return response.data;
 }
 
 export async function MovieCreditsFetch(id) {
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=125725f49ad2ae69609a1a5a9c4211d9&language=en-US`);
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=b4a9604d912adcda3dc38f0ba7bfa255&language=en-US`);
 
     return response.data;
 }
 
 export async function MovieReviewsFetch(id) {
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=125725f49ad2ae69609a1a5a9c4211d9&language=en-US&page=1`)
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=b4a9604d912adcda3dc38f0ba7bfa255&language=en-US&page=1`)
 
     return response.data.results;
 }
